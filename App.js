5cc0bd01c20a0a874b9a392597d0f73032135dbe
ihor-vip <vip20140902@gmail.com>
2023-03-01 18:47:06 +0200

    create stylesheet object

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text>Another piece of text!</Text>
-      </View>
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.dummyText}>Another piece of text!</Text>
+            </View>
 
-      <Text
-          style={{margin: 16, borderWidth: 2, borderColor: 'red', padding: 16}}
-      >
-        Hello world!
-      </Text>
+            <Text style={styles.dummyText}>Hello world!</Text>
 
-      <Button title='Tap me!' />
-    </View>
-  );
+            <Button title='Tap me!'/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    dummyText: {
+        margin: 16,
+        padding: 16,
+        borderWidth: 2,
+        borderColor: 'red'
+    }
 });
