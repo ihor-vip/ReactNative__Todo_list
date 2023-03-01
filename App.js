77f87d325f7a7cbff956cce8afe2675f5733225a
ihor-vip <vip20140902@gmail.com>
2023-03-01 18:37:32 +0200

    first init, practice with inline styles

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+
+      <Text
+          style={{margin: 16, borderWidth: 2, borderColor: 'red', padding: 16}}
+      >
+        Hello world!
+      </Text>
+
+      <Button title='Tap me!' />
     </View>
   );
 }
