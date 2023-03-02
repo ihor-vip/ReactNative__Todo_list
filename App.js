3c7b3de63a0a2396250479554eb544968407286c
ihor-vip <vip20140902@gmail.com>
2023-03-02 14:52:05 +0200

    adding scroll view to app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useState} from "react";
 
 export default function App() {
@@ -28,15 +28,17 @@ export default function App() {
                 <Button title='Add Goal' onPress={addGoalHandler}/>
             </View>
             <View style={styles.goalsContainer}>
-                {courseGoals.map((goal) => (
-                    <View key={goal} style={styles.goalItem}>
-                        <Text style={styles.goalText}>
-                            {goal}
-                        </Text>
-                    </View>
-                ))}
+                <ScrollView>
+                    {courseGoals.map((goal) => (
+                        <View key={goal} style={styles.goalItem}>
+                            <Text style={styles.goalText}>
+                                {goal}
+                            </Text>
+                        </View>
+                    ))}
+                </ScrollView>
+            </View>
         </View>
-</View>
 )
     ;
 }
