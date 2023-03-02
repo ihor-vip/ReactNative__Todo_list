16a2451e32825fd2303f446c98f4e72895fee43a
ihor-vip <vip20140902@gmail.com>
2023-03-02 15:14:37 +0200

    fixed

--- App.js
+++ App.js
@@ -37,9 +37,9 @@ export default function App() {
                             </View>
                         )
                     }}
-                    keyExtractor={(item, index) => (
+                    keyExtractor={(item, index) => {
                         return item.id;
-                    )}
+                    }}
                     alwaysBounceVertical={false}
                 />
             </View>
